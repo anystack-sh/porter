79801d264031be21df9c94d48fc3324c13fce0a5
Scott Carpenter <scott@goquadra.com>
2023-10-25 14:41:38 -0600

    Add a `--services` option to the `tail` command

--- app/Commands/TailCommand.php
+++ app/Commands/TailCommand.php
@@ -5,7 +5,9 @@ namespace App\Commands;
 use App\Repositories\ConfigRepository;
 use App\Repositories\SupervisordRepository;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 
 class TailCommand extends Command
 {
@@ -14,7 +16,10 @@ class TailCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'tail {app?} {--all}';
+    protected $signature = 'tail
+                            {app?}
+                            {--services= : Comma-separated list of service indexes (e.g. 0,1) or service names to tail}
+                            {--all}';
 
     /**
      * The description of the command.
@@ -37,13 +42,7 @@ class TailCommand extends Command
         $appServices = $supervisordRepository->getAllProcessInfo()->where('group', $app);
 
         if ($this->option('all') === false) {
-            $servicesToTail = $this->choice(
-                'Which service do you want to tail?',
-                $appServices->pluck('name')->toArray(),
-                null,
-                null,
-                true
-            );
+            $servicesToTail = $this->resolveServicesToTail($appServices);
         }
 
         $files = $appServices
@@ -72,6 +71,44 @@ class TailCommand extends Command
         $app = $config->apps->firstWhere('dir', getcwd())['name'] ?? null;
 
         return $app !== null ? str($app)->slug() : null;
+    }
+
+    private function resolveServicesToTail(Collection $appServices): mixed
+    {
+        return $this->getServicesFromOption($appServices) ??
+            $this->choice(
+                'Which service do you want to tail?',
+                $appServices->pluck('name')->toArray(),
+                null,
+                null,
+                true
+            );
+    }
+
+    private function getServicesFromOption(Collection $appServices): mixed
+    {
+        $services = $this->option('services');
+
+        if (empty($services)) {
+            return null;
+        }
+
+        return Str::of($services)
+            ->explode(',')
+            ->map(function ($service) use ($appServices) {
+                if (is_numeric($service) && isset($appServices[$service])) {
+                    return $appServices[$service]['name'];
+                }
+
+                if ($appServices->contains('name', $service)) {
+                    return $service;
+                }
+
+                throw new InvalidArgumentException("Service \"$service\" is invalid");
+            })
+            ->filter()
+            ->unique()
+            ->toArray() ?: null;
     }
 
     private function tail(Collection $files)
